0d66e5d0f95ea4a1c583af00ee20f816ced5612c
VANDRA MAXENCE p1608372 <maxence.vandra@etu.univ-lyon1.fr>
2019-09-30 17:19:09 +0200

    Ajout du projet Maven, avec la dépendance Junit et le plugin Checkstyle

---
+++ parent_project/src/main/java/course_evaluation/parent_project/App.java
@@ -0,0 +1,11 @@
+package course_evaluation.parent_project;
+
+/**
+ * Hello world.
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
