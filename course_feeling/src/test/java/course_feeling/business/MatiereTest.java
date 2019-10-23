3ecf5d19e19f92d719d771c97d751a957cf03125
Richard Bruneau <richard.bruneau@etu.univ-lyon1.fr>
2019-10-23 17:26:37 +0200

    Mise à jour version d'JUnit

---
+++ course_feeling/src/test/java/course_feeling/business/MatiereTest.java
@@ -0,0 +1,13 @@
+package course_feeling.business;
+
+import org.junit.Test;
+
+public class MatiereTest {
+    
+    @Test
+    public void matiereTestEcho(){
+        System.out.println("bipbip");
+        //Thread.sleep(5000);
+    
+    }
+}
