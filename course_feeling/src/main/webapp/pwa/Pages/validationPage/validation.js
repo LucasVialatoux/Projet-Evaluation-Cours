a08e8eb91696204639600cad7f1b2c5c50d0f0f8
Pierre Vaslin <pierre.vaslin@etu.univ-lyon1.fr>
2019-10-25 18:48:30 +0200

    FONT-FIX:correction de petit bug / style

--- course_feeling/src/main/webapp/pwa/Pages/validationPage/validation.js
+++ course_feeling/src/main/webapp/pwa/Pages/validationPage/validation.js
@@ -1,10 +1,12 @@
 var URL_PAGE_CODE = "../../index.html";
 var NAME_SUBJECT = "nameSbj";
-localStorage.setItem(NAME_SUBJECT, "Nom de la matière"); //TEMPO
 
 $(function() {
     /* Récupération du nom de métier depuis le stockage local*/  
     let nameSbj = localStorage.getItem(NAME_SUBJECT);
+    if (nameSbj == null) {
+        nameSbj = "Matière par défaut";
+    }
     if (nameSbj != null) {
         $("#nav_title").text(nameSbj);
         $("#discipline_title").text(nameSbj); 
