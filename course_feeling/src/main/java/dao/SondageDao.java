2b85012ad84a0c92b46e315f67f808a71090acaf
VANDRA MAXENCE p1608372 <maxence.vandra@etu.univ-lyon1.fr>
2019-11-01 14:16:45 +0100

    JAVA-UPGRADE: Ajout de fonction dans l'interface sondage DAO

--- course_feeling/src/main/java/dao/SondageDao.java
+++ course_feeling/src/main/java/dao/SondageDao.java
@@ -1,9 +1,17 @@
 package dao;
 
+import java.util.Map;
+
 import business.Ressenti;
 
 public interface SondageDao {
     public String getMatiereOfSondage(String codeSondage);
     
-    public boolean ajouterRessenti(String sondage, Ressenti ressenti);
+    public void ajouterRessenti(String sondage, Ressenti ressenti);
+    
+    public void ajouterSondage(String idProf, String idMatiere);
+    
+    public String getCode(int id);
+    
+    public Map<Ressenti, Integer> getResultat(int id);
 }
