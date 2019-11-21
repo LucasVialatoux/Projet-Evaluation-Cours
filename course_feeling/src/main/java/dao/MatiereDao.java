335ec2d9630282b712efab51bdf34eaeaf566c2b
VANDRA MAXENCE p1608372 <maxence.vandra@etu.univ-lyon1.fr>
2019-11-21 14:32:14 +0100

    JAVA-UPGRADING : Mise à jour de DAO de matière

--- course_feeling/src/main/java/dao/MatiereDao.java
+++ course_feeling/src/main/java/dao/MatiereDao.java
@@ -1,19 +1,44 @@
 package dao;
 
+import java.util.List;
+
 import business.Matiere;
 import business.MatiereResultat;
 
 public interface MatiereDao {
-    // Ajout matière (idprof, nommat)
+    
+    /**
+     * Ajout d'une matière pour un prof.
+     * @param idProf : identifiant du prof.
+     * @param nomMat : nom de la matière.
+     * @throws MatiereDaoException
+     */
     public void ajoutMatiere(String idProf, String nomMat) throws MatiereDaoException;
     
-    // Supprimer une matière
-    public void supprimerMatiere(String idProf, String numMat) throws MatiereDaoException;
+    /**
+     * Suppression d'une matière d'un prof.
+     * @param idProf : identifiant du prof.
+     * @param nomMat : nom de la matière.
+     * @throws MatiereDaoException
+     */
+    public void supprimerMatiere(String idProf, String nomMat) throws MatiereDaoException;
     
-    // Récupérer les matières (avec sondages dedans) (idprof)
-    public Matiere getMatieres(String idProf) throws MatiereDaoException;
+    /**
+     * Récupère les resultats de sondage des matières d'un prof.
+     * @param idProf : identifiant du prof.
+     * @return
+     * @throws MatiereDaoException
+     */
+    public List<Matiere> getMatieres(String idProf) throws MatiereDaoException;
     
-    // Récupérer resultat des matières
-    public MatiereResultat getResultat() throws MatiereDaoException;
+    /**
+     * Récupère les résultats de sondage d'une matière.
+     * @param idProf : identifiant du prof.
+     * @param nomMat : nom de la matière.
+     * @return
+     * @throws MatiereDaoException
+     */
+    public MatiereResultat getResultat(String idProf, String nomMat) throws MatiereDaoException;
+    
 
 }
