ea64d7cc3d7259e6ae13f7b01eb23047117458c3
Richard Bruneau <richard.bruneau@etu.univ-lyon1.fr>
2019-11-22 11:03:38 +0100

    TEST: getDate() est maintenant testé

--- course_feeling/src/test/java/business/SondageTest.java
+++ course_feeling/src/test/java/business/SondageTest.java
@@ -14,10 +14,16 @@ public class SondageTest {
     public  SondageTest() {
         sondage1 = new Sondage();
         sondage1.setId(1);
+        sondage1.setDate(22112019);
     }
 
     @Test
     public void SondageTestGetId() {
         assertEquals(1, sondage1.getId());
+    }
+
+    @Test
+    public void SondageTestGetDate() {
+        assertEquals(22112019, sondage1.getDate());
     }
 }
