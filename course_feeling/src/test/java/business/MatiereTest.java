6f8509a2e3a2d284c86b3a6fd3bacc7bfaaf0238
Richard Bruneau <richard.bruneau@etu.univ-lyon1.fr>
2019-11-22 08:51:58 +0100

    TEST: Rajout de test pour couvrir la classe à 100%

--- course_feeling/src/test/java/business/MatiereTest.java
+++ course_feeling/src/test/java/business/MatiereTest.java
@@ -8,6 +8,10 @@ import business.Sondage;
 
 import static org.junit.Assert.assertEquals;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MatiereTest {
 
     Matiere matiereTest1;
@@ -29,5 +33,24 @@ public class MatiereTest {
     public void MatiereTestSetGetName() {
         matiereTest1.setName("UE-Test");
         assertEquals("UE-Test", matiereTest1.getName());
+    }
+
+    @Test
+    public void MatiereTestGetSondageEtSetSondage() {
+        List<Sondage> listeDeSondageTest = new ArrayList();
+
+        Sondage s = new Sondage();
+        s.setId(154);
+        s.setDate(173837829);
+        listeDeSondageTest.add(s);
+
+        Sondage s2 = new Sondage();
+        s.setId(345);
+        s.setDate(89787897);
+        listeDeSondageTest.add(s2);
+
+        matiereTest1.setSondages(listeDeSondageTest);
+
+        assertEquals(listeDeSondageTest, matiereTest1.getSondages());
     }
 }
