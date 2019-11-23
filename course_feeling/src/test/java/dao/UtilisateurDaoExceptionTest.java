d1afedf91788aae4ad3ba4f30ddb41c1a1da1390
Richard Bruneau <richard.bruneau@etu.univ-lyon1.fr>
2019-11-23 10:48:07 +0100

    TEST: Correction des imports

--- course_feeling/src/test/java/dao/UtilisateurDaoExceptionTest.java
+++ course_feeling/src/test/java/dao/UtilisateurDaoExceptionTest.java
@@ -1,23 +1,6 @@
 package dao;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-
-import static org.mockito.Mockito.*;
 
 import dao.SondageDaoMockImpl;
 
