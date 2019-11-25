10bcf87fda40b232d0d88ae26f5d0da6e7dc50d2
Richard Bruneau <richard.bruneau@etu.univ-lyon1.fr>
2019-11-25 16:35:07 +0100

    TEST : Début des tests JS

---
+++ PWANode/www/test/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('First Test', function() {
+
+    it('should do something', function(){
+        var a  = 3;
+        assert.equal(a*2, 6)
+    })
+
+
+})
